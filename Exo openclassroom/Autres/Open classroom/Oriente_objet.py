48dd8da1020076228be044b25fa3c8707ddcb87c
IDarkParadiseI <dierickxsimon198@gmail.com>
2024-10-03 17:35:09 +0200

    Chapitre 1 + exo

---
+++ Exo openclassroom/Autres/Open classroom/Oriente_objet.py
@@ -0,0 +1,56 @@
+# Ecrivez une classe Python
+
+    #Exemple 
+    
+# class Rectangle:
+#     width = 3
+#     heigth = 5
+    
+#     def calculate_area(self):
+#         return self.width * self.heigth
+
+
+# class Cake:
+    
+#     def __init__(self, flavor):
+#         self.flavor = flavor
+        
+#     def cut_in_part(self):
+#         print("Le gateau est coupé en 4 parts !")
+
+
+# ----- Exercice ----- 
+
+# class Boite:
+#     def __init__(self):
+#         self.tools = []
+        
+#     def addTools(self, tools):
+#         pass
+    
+#     def remove_tools(self, tools):
+#         pass
+    
+# class TourneVis():
+#     def __inti__(self, size):
+#         self.size = size
+    
+#     def serrer(self, screw):
+#         pass
+    
+#     def devisser(self, screw):
+#         pass
+    
+# class Marteau():
+    
+#     def __init__(self, color='red'):
+#         self.color = color
+        
+#     def paint(self, color):
+#         pass
+    
+#     def hammer_in(self, nail):
+#         pass
+    
+#     def remove(self, nail):
+#         pass
